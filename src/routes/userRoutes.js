4dc2f44772b59df91717cfd28355e4a449141d6b
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2025-04-02 18:52:13 -0400

    Bug fix attempt

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const passport = require('../config/passport-jwt');
 const passportJWT = passport.authenticate('jwt', { session: false });
+const cors = require('cors');
 
 router.post('/signup', userController.createUser);
 router.post('/login', userController.login);
@@ -11,7 +12,7 @@ router.post(
   '/resend-email-verification',
   userController.resendEmailVerification
 );
-router.post('/guest', userController.createGuest);
+router.post('/guest', cors(), userController.createGuest);
 //router.post('/create-admin', userController.createUserByAdmin);
 
 router.get('/:id', passportJWT, userController.getUserById);
